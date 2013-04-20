123311efccc296d45874885560e09784f0cc6dcc
Chris Cat <quanct@gmail.com>
2013-04-20 15:43:37 -0700

    Add a mock sign-up form to the home page.

---
+++ Guardfile
@@ -0,0 +1,5 @@
+# More info at https://github.com/guard/guard#readme
+
+guard :livereload do
+    watch(%r{.+\.(jade|html|css|js|jpg|gif|png)$})
+end
--- routes/user.js
+++ routes/user.js
@@ -6,3 +6,29 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+exports.signup = function(req, res) {
+  //var mongoose = require('mongoose');
+  //mongoose.connect('mongodb://localhost/touchbase');
+  //var db = mongoose.connection;
+  //db.on('error', console.error.bind(console, 'connection error:'));
+  //db.once('open, function() {
+    //var UserSchema = mongoose.Schema({
+      //fName: String
+    //, lName: String
+    //, email: String
+    //, password: String
+    //});
+
+    //var User = mongoose.model('User', UserSchema);
+
+
+  //});
+  res.render('new-user', {
+    title: 'Welcome'
+  , fName: req.body.fName
+  , lName: req.body.lName
+  , email: req.body.email
+  , password: req.body.password
+  });
+};
