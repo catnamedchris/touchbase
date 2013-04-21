efaa2d6522aab205e651415a7d705d47225f0011
Chris Cat <quanct@gmail.com>
2013-04-20 23:17:43 -0700

    Add db write on sign-up submission.

--- routes/user.js
+++ routes/user.js
@@ -8,27 +8,29 @@ exports.list = function(req, res){
 };
 
 exports.signup = function(req, res) {
-  //var mongoose = require('mongoose');
-  //mongoose.connect('mongodb://localhost/touchbase');
-  //var db = mongoose.connection;
-  //db.on('error', console.error.bind(console, 'connection error:'));
-  //db.once('open, function() {
-    //var UserSchema = mongoose.Schema({
-      //fName: String
-    //, lName: String
-    //, email: String
-    //, password: String
-    //});
-
-    //var User = mongoose.model('User', UserSchema);
-
-
-  //});
-  res.render('new-user', {
-    title: 'Welcome'
-  , fName: req.body.fName
-  , lName: req.body.lName
-  , email: req.body.email
-  , password: req.body.password
+  var mongoose = require('mongoose');
+  mongoose.connect('mongodb://localhost/touchbase');
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    var UserSchema = mongoose.Schema({
+      fName: String
+    , lName: String
+    , email: String
+    , password: String
+    });
+    var User = mongoose.model('User', UserSchema);
+    var userData = {
+      fName: req.body.fName
+    , lName: req.body.lName
+    , email: req.body.email
+    , password: req.body.password
+    };
+    var newUser = new User(userData);
+    newUser.save(function(err) {
+      userData.title = 'Welcome';
+      if (err) console.log(err);
+      res.render('new-user', userData);
+    });
   });
 };
